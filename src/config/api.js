00500e763a487c1d4d961c68352189ae1191d01c
PhongNT <phongnt@kaopiz.com>
2023-12-01 21:24:58 +0700

    call api from themoviedb.org

---
+++ src/config/api.js
@@ -0,0 +1,123 @@
+const headUrl = 'https://api.themoviedb.org/3/movie'
+const API_KEY = 'e9652b73c86b0fcf38b993217c00e7b4'
+
+export const fetchPopularMovies = async () => {
+  try {
+    const apiUrl = `${headUrl}/popular?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const fetchNowPlayingMovies = async (page) => {
+  try {
+    const apiUrl = `${headUrl}/now_playing?api_key=${API_KEY}&page=${page}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const fetchTopRatedMovies = async () => {
+  try {
+    const apiUrl = `${headUrl}/top_rated?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const fetchUpcomingMovies = async () => {
+  try {
+    const apiUrl = `${headUrl}/upcoming?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const fetchMovieDetails = async (movieId) => {
+  try {
+    const apiUrl = `${headUrl}/${movieId}?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export const fetchMovieCredits = async (movieId) => {
+  try {
+    const apiUrl = `${headUrl}/${movieId}/credits?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export const fetchMovieVideo = async (movieId) => {
+  try {
+    const apiUrl = `${headUrl}/${movieId}/videos?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export const fetchMovieSimilar = async (movieId) => {
+  try {
+    const apiUrl = `${headUrl}/${movieId}/similar?api_key=${API_KEY}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+    return null
+  }
+}
+
+export const searchMovie = async (query, page) => {
+  try {
+    const encodedQuery = encodeURIComponent(query)
+    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${encodedQuery}&page=${page}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
+
+export const nextListMovie = async (page) => {
+  try {
+    const apiUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&page=${page}`
+    const response = await fetch(apiUrl)
+    const data = await response.json()
+    return data.results
+  } catch (error) {
+    console.log(error)
+    return []
+  }
+}
