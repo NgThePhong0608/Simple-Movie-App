ded074773c1f8cdab00cf8f1c0e853e85b15c984
PhongNT <phongnt@kaopiz.com>
2023-12-01 21:22:08 +0700

    create header and footer component

---
+++ src/components/footer/FooterLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="text-center pb-10">
+    <q class="text-white tracking-widest text-lg italic">
+      The UI is built by Evondev, and the developer is me - PhongNT.
+    </q>
+  </div>
+</template>
